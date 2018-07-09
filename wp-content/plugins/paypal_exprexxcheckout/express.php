a54005462e63e0fa80123a2f229bc4a31c088182
echizenyayota <virtualmisso30@gmail.com>
2018-07-09 15:28:45 +0900

    コンストラクタの作成

--- wp-content/plugins/paypal_exprexxcheckout/express.php
+++ wp-content/plugins/paypal_exprexxcheckout/express.php
@@ -18,6 +18,14 @@ class PayPal_ExpressCheckout {
   // プロパティ（フィールドコールバックで使用される値を保持）
   private $options;
 
+  // コンストラクタによる初期化
+  public function __construct() {
+    // checkout.jsの読み込み
+    add_action( 'wp_enqueue_scripts', array($this, 'paypal_scripts' );
+    add_action( 'admin_menu', array($this, 'paypalexpresscheckout_add_admin_menu') );
+    add_action( 'admin_init', array($this, 'register_paypalsettings') );
+  }
+
 
 
 }
